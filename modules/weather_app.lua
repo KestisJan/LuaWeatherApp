8965530333557d64bbb21946a8a54e8e892b0d83
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-01-30 10:20:00 +0200

    Implement getWeather method in WeatherAPI

--- modules/weather_app.lua
+++ modules/weather_app.lua
@@ -81,3 +81,40 @@ function WeatherAPI:getCoordinates(option, input)
     return result
 end
 
+-- Get weather information based on geographical coordinates.
+-- @param lat (number) - The latitude coordinate.
+-- @param lon (number) - The longitude coordinate
+-- @return result (table) - The weather information as a Lua table.
+
+function WeatherAPI:getWeather(lat, lon)
+    -- Constructing the query string with necessary parameters
+    local queryString = string.format("lat=%s&lon=%s&lang=%s&appid=%s&mode=%s", lat, lon, self.lang, self.apiKey, self.mode )
+    local fullUrl = self.weatherApiUrl .. "?" .. queryString
+
+    -- Creating an HTTP request object
+    local req = http_request.new_from_uri(fullUrl)
+
+    -- Using pcall to catch errors during the HTTP request
+    local success, headers, stream = pcall(req.go, req)
+    if not success then
+        -- Retrieving an error table if the HTTP request fails
+        return { error = "HTTP request failed."}
+    end
+
+    -- Reading the response body
+    local body = assert(stream:get_body_as_string())
+
+    -- Checking if the HTTP status is not 200 (OK)
+    if headers:get(":status") ~= "200" then
+        -- Returning an error table with the error message
+        return { error = body }
+    end
+
+    -- Decoding the JSON responsive body into a Lua table
+    local result = json.decode(body)
+
+    -- Returning the result table
+    return result
+end
+
+return WeatherAPI
